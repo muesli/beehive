693dcecc7bb6d7b3f5ded8febf7acf21ceb50383
Christian Muehlhaeuser <muesli@gmail.com>
2014-06-23 22:54:31 +0200

    * Initial commit.

---
+++ beehive.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	_ "log"
+
+	// "github.com/muesli/beehive/app"
+
+	/* "github.com/muesli/beehive/commands"
+	_ "github.com/muesli/beehive/commands/send" */
+
+	/* "github.com/muesli/beehive/msgsystem"
+	_ "github.com/muesli/beehive/msgsystem/web/hooks/jira" */)
+
+func main() {
+	// Parse command-line args for all registered sub modules
+	// app.Run()
+
+	// Initialize commands and messaging sub-systems
+	// commands.StartCommands()
+	// msgsystem.StartSubSystems()
+
+	fmt.Println("Beehive starting up...")
+
+	// Keep app alive
+	/* ch := make(chan bool)
+	<-ch */
+}
