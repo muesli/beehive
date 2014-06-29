d03c237d7e6a95e53778be68b5b4633b7d3a9cc4
grindhold <grindhold@gmx.net>
2014-06-29 18:58:55 +0200

    fixed jenkinsbeeissue with remembering last state

--- modules/jenkinsbee/jenkinsbee.go
+++ modules/jenkinsbee/jenkinsbee.go
@@ -21,116 +21,119 @@
 package jenkinsbee
 
 import (
-    "github.com/muesli/beehive/modules"
-    "log"
-    "io/ioutil"
-    "net/http"
-    "time"
-    "encoding/json"
+	"encoding/json"
+	"github.com/muesli/beehive/modules"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
 )
 
 type JenkinsBee struct {
-    modules.Module
+	modules.Module
 
-    url     string
-    Jobs map[string]Job `json:"jobs"`
-    eventChan chan modules.Event
+	url       string
+	Jobs      map[string]Job `json:"jobs"`
+	eventChan chan modules.Event
 }
 
 type report struct {
-    Jobs []Job `json:"jobs"`
+	Jobs []Job `json:"jobs"`
 }
 
 type Job struct {
-    Name string `json:"name"`
-    Url string `json:"url"`
-    Color string `json:"color"`
+	Name  string `json:"name"`
+	Url   string `json:"url"`
+	Color string `json:"color"`
 }
 
-func (mod *JenkinsBee) announceStatusChange (j Job){
-    event := modules.Event{
-        Bee: mod.Name(),
-        Name: "statuschange",
-        Options: []modules.Placeholder{
-            modules.Placeholder{
-                Name : "name",
-                Type : "string",
-                Value: j.Name,
-            },
-            modules.Placeholder{
-                Name : "url",
-                Type:  "string",
-                Value: j.Url,
-            },
-            modules.Placeholder{
-                Name : "status",
-                Type: "string",
-                Value: j.Color,
-            },
-        },
-    }
-    mod.eventChan <- event
+func (mod *JenkinsBee) announceStatusChange(j Job) {
+	event := modules.Event{
+		Bee:  mod.Name(),
+		Name: "statuschange",
+		Options: []modules.Placeholder{
+			modules.Placeholder{
+				Name:  "name",
+				Type:  "string",
+				Value: j.Name,
+			},
+			modules.Placeholder{
+				Name:  "url",
+				Type:  "string",
+				Value: j.Url,
+			},
+			modules.Placeholder{
+				Name:  "status",
+				Type:  "string",
+				Value: j.Color,
+			},
+		},
+	}
+	mod.eventChan <- event
 }
 
-func (mod *JenkinsBee) Run (cin chan modules.Event) {
-    mod.eventChan = cin
-    for {
-        resp, err := http.Get(mod.url+"/api/json")
-        if err != nil {
-            log.Println("Could not call API on "+mod.url+"/api/json")
-            time.Sleep(5*time.Second)
-            continue
-        }
-        body, err := ioutil.ReadAll(resp.Body)
-        if err!=nil{
-            log.Println("Could not read data of API-Call")
-            time.Sleep(5*time.Second)
-            continue
-        }
-        rep := new (report)
-        err = json.Unmarshal(body, &rep)
-        if err!=nil{
-            log.Println("Failed to unmarshal JSON")
-            time.Sleep(5*time.Second)
-            continue
-        }
-        for job := range rep.Jobs {
-            jobmap := make(map[string]Job)
-            if oldState, ok := mod.Jobs[rep.Jobs[job].Name] ; !ok {
-                // There is no record of this job
-                mod.announceStatusChange(rep.Jobs[job])
-            } else {
-                // There exists a record of this job
-                if oldState.Color != rep.Jobs[job].Color {
-                    // The status is different from last time
-                    mod.announceStatusChange(rep.Jobs[job])
-                }
-            }
-            jobmap[rep.Jobs[job].Name] = rep.Jobs[job]
-        }
-        time.Sleep(5*time.Second)
-    }
+func (mod *JenkinsBee) Run(cin chan modules.Event) {
+	mod.eventChan = cin
+	for {
+		resp, err := http.Get(mod.url + "/api/json")
+		if err != nil {
+			log.Println("Could not call API on " + mod.url + "/api/json")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println("Could not read data of API-Call")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+		rep := new(report)
+		err = json.Unmarshal(body, &rep)
+		if err != nil {
+			log.Println("Failed to unmarshal JSON")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+
+		jobmap := make(map[string]Job)
+		for job := range rep.Jobs {
+			if oldState, ok := mod.Jobs[rep.Jobs[job].Name]; !ok {
+				// There is no record of this job
+				mod.announceStatusChange(rep.Jobs[job])
+			} else {
+				// There exists a record of this job
+				if oldState.Color != rep.Jobs[job].Color {
+					// The status is different from last time
+					mod.announceStatusChange(rep.Jobs[job])
+				}
+			}
+			jobmap[rep.Jobs[job].Name] = rep.Jobs[job]
+		}
+		mod.Jobs = jobmap
+		time.Sleep(5 * time.Second)
+	}
 }
 
-func (mod *JenkinsBee) triggerBuild (jobname string) {
-    if _, err := http.Get(mod.url+"/job/"+jobname+"/build") ; err != nil {
-        log.Println("Could not trigger build")
-    }
-    return
+func (mod *JenkinsBee) triggerBuild(jobname string) {
+	if _, err := http.Get(mod.url + "/job/" + jobname + "/build"); err != nil {
+		log.Println("Could not trigger build")
+	}
+	return
 }
 
-func (mod *JenkinsBee) Action (action modules.Action) []modules.Placeholder {
-    outs :=[]modules.Placeholder{}
-    switch action.Name {
-        case "trigger":
-            jobname := ""
-            for _, opt := range action.Options {
-                if opt.Name == "job" {
-                    jobname = opt.Value.(string)
-                }
-            }
-            mod.triggerBuild(jobname)
-        default: return outs
-    }
-    return outs
+func (mod *JenkinsBee) Action(action modules.Action) []modules.Placeholder {
+	outs := []modules.Placeholder{}
+	switch action.Name {
+	case "trigger":
+		jobname := ""
+		for _, opt := range action.Options {
+			if opt.Name == "job" {
+				jobname = opt.Value.(string)
+			}
+		}
+		mod.triggerBuild(jobname)
+	default:
+		return outs
+	}
+	return outs
 }
--- modules/jenkinsbee/jenkinsbeefactory.go
+++ modules/jenkinsbee/jenkinsbeefactory.go
@@ -21,86 +21,86 @@
 package jenkinsbee
 
 import (
-    "github.com/muesli/beehive/modules"
+	"github.com/muesli/beehive/modules"
 )
 
 type JenkinsBeeFactory struct {
 }
 
-func (factory *JenkinsBeeFactory) New (name, description string, options modules.BeeOptions) modules.ModuleInterface {
-    bee := JenkinsBee {
-        url: options.GetValue("url").(string),
-    }
-    bee.Module = modules.Module{name, factory.Name(), description}
-    return &bee
+func (factory *JenkinsBeeFactory) New(name, description string, options modules.BeeOptions) modules.ModuleInterface {
+	bee := JenkinsBee{
+		url: options.GetValue("url").(string),
+	}
+	bee.Module = modules.Module{name, factory.Name(), description}
+	return &bee
 }
 
 func (factory *JenkinsBeeFactory) Name() string {
-    return "jenkinsbee"
+	return "jenkinsbee"
 }
 
 func (factory *JenkinsBeeFactory) Description() string {
-    return "A bee that triggers and reads info from Jenkins-Builds"
+	return "A bee that triggers and reads info from Jenkins-Builds"
 }
 
 func (factory *JenkinsBeeFactory) Options() []modules.BeeOptionDescriptor {
-    opts := []modules.BeeOptionDescriptor{
-        modules.BeeOptionDescriptor{
-            Name: "url",
-            Description: "The url the jenkins-installation is reachable at",
-            Type: "string",
-        },
-    }
-    return opts
+	opts := []modules.BeeOptionDescriptor{
+		modules.BeeOptionDescriptor{
+			Name:        "url",
+			Description: "The url the jenkins-installation is reachable at",
+			Type:        "string",
+		},
+	}
+	return opts
 }
 
 func (factory *JenkinsBeeFactory) Events() []modules.EventDescriptor {
-    events := []modules.EventDescriptor{
-        modules.EventDescriptor{
-            Namespace: factory.Name(),
-            Name:   "statuschange",
-            Description: "the status of a job has changed",
-            Options: []modules.PlaceholderDescriptor {
-                modules.PlaceholderDescriptor {
-                    Name: "name",
-                    Description: "Name of the job",
-                    Type: "string",
-                },
-                modules.PlaceholderDescriptor {
-                    Name: "status",
-                    Description: "Current status of the job ('red' or 'blue')",
-                    Type: "string",
-                },
-                modules.PlaceholderDescriptor {
-                    Name: "url",
-                    Description: "URL of the affected job",
-                    Type : "string",
-                },
-            },
-        },
-    }
-    return events
+	events := []modules.EventDescriptor{
+		modules.EventDescriptor{
+			Namespace:   factory.Name(),
+			Name:        "statuschange",
+			Description: "the status of a job has changed",
+			Options: []modules.PlaceholderDescriptor{
+				modules.PlaceholderDescriptor{
+					Name:        "name",
+					Description: "Name of the job",
+					Type:        "string",
+				},
+				modules.PlaceholderDescriptor{
+					Name:        "status",
+					Description: "Current status of the job ('red' or 'blue')",
+					Type:        "string",
+				},
+				modules.PlaceholderDescriptor{
+					Name:        "url",
+					Description: "URL of the affected job",
+					Type:        "string",
+				},
+			},
+		},
+	}
+	return events
 }
 
 func (factory *JenkinsBeeFactory) Actions() []modules.ActionDescriptor {
-    actions := []modules.ActionDescriptor{
-        modules.ActionDescriptor{
-            Namespace: factory.Name(),
-            Name: "trigger",
-            Description: "Trigger a build on this jenkins machine",
-            Options: []modules.PlaceholderDescriptor{
-                modules.PlaceholderDescriptor{
-                    Name: "job",
-                    Description: "Name of the job on which to trigger a build",
-                    Type: "string",
-                },
-            },
-        },
-    }
-    return actions
+	actions := []modules.ActionDescriptor{
+		modules.ActionDescriptor{
+			Namespace:   factory.Name(),
+			Name:        "trigger",
+			Description: "Trigger a build on this jenkins machine",
+			Options: []modules.PlaceholderDescriptor{
+				modules.PlaceholderDescriptor{
+					Name:        "job",
+					Description: "Name of the job on which to trigger a build",
+					Type:        "string",
+				},
+			},
+		},
+	}
+	return actions
 }
 
-func init () {
-    f := JenkinsBeeFactory{}
-    modules.RegisterFactory(&f)
+func init() {
+	f := JenkinsBeeFactory{}
+	modules.RegisterFactory(&f)
 }
