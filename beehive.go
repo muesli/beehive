5526e52241fadcd3616af6cbab64ff634398bed4
Christian Muehlhaeuser <muesli@gmail.com>
2014-07-27 17:06:44 +0200

    * Switch to upstream-repo.

--- beehive.go
+++ beehive.go
@@ -50,7 +50,7 @@ import (
 	_ "github.com/muesli/beehive/modules/spaceapibee"
 	_ "github.com/muesli/beehive/modules/htmlextractbee"
 	_ "github.com/muesli/beehive/modules/efabee"
-    _ "github.com/waaaaargh/beehive/modules/twitterbee"
+	_ "github.com/muesli/beehive/modules/twitterbee"
 )
 
 var (
