8127c4c8b48cb0cb82292824cebeb6ebb348e562
Christian Muehlhaeuser <muesli@gmail.com>
2014-08-02 01:40:42 +0200

    * Fixed coverage script.

--- admin/coverage.sh
+++ admin/coverage.sh
@@ -5,7 +5,7 @@ go test -coverprofile /tmp/beehive_coverage2.part ./filters/endswith
 go test -coverprofile /tmp/beehive_coverage3.part ./filters/contains
 go test -coverprofile /tmp/beehive_coverage4.part ./filters/equals
 
-go test -coverprofile /tmp/beehive_coverage4.part ./modules/cronbee/cron
+go test -coverprofile /tmp/beehive_coverage4.part ./bees/cronbee/cron
 
 echo "mode: set" > /tmp/beehive_coverage.out
 grep -h -v "mode: set" /tmp/beehive_coverage*.part >> /tmp/beehive_coverage.out
