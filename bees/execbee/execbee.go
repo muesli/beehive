8cdb5b4b33dfe899a255e085620bd341b3e2ed73
Dominik Schmidt <domme@tomahawk-player.org>
2015-04-17 08:26:46 +0200

    Recover from PR submitted and merged at 8:15am

--- bees/execbee/execbee.go
+++ bees/execbee/execbee.go
@@ -83,19 +83,15 @@ func (mod *ExecBee) Action(action bees.Action) []bees.Placeholder {
 						}
 					}()
 
-					cmd.Start()
 					err = cmd.Start()
 					if err != nil {
 						fmt.Fprintln(os.Stderr, "Error starting Cmd", err)
-						os.Exit(1)
 					}
 
 					err = cmd.Wait()
 					if err != nil {
 						fmt.Fprintln(os.Stderr, "Error waiting for Cmd", err)
-						os.Exit(1)
 					}
-					cmd.Wait()
 
 					ev := bees.Event{
 						Bee:  mod.Name(),
