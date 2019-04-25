cf39c6f06202808c8eee102ab3b00a0c63337ac8
James Mills <1290234+prologic@users.noreply.github.com>
2019-04-25 13:57:28 +1000

    Added goreleaser config and tool to help make releases easier

---
+++ tools/release.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if ! command -v goreleaser; then
+  echo "goreleaser not found"
+  exit 1
+fi
+
+# Get the highest tag number
+VERSION="$(git describe --abbrev=0 --tags)"
+VERSION=${VERSION:-'0.0.0'}
+
+# Get number parts
+MAJOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+MINOR="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+PATCH="${VERSION%%.*}"; VERSION="${VERSION#*.}"
+
+# Increase version
+PATCH=$((PATCH+1))
+
+TAG="${1}"
+
+if [ "${TAG}" = "" ]; then
+  TAG="${MAJOR}.${MINOR}.${PATCH}"
+fi
+
+echo "Releasing ${TAG} ..."
+
+git tag -a -s -m "Relase ${TAG}" "${TAG}"
+git push --tags
+goreleaser release --rm-dist
