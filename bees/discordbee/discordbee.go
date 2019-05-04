56669bbc5f36844dfcb2fa4b209b84800c22f650
CalmBit <EB5473@gmail.com>
2019-05-04 05:07:21 -0400

    Discord pt1

---
+++ bees/discordbee/discordbee.go
@@ -0,0 +1,143 @@
+/*
+ *    Copyright (C) 2019      CalmBit
+ *                  2014-2019 Christian Muehlhaeuser
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Authors:
+ *      CalmBit <calmbit@posto.net>
+ *      Christian Muehlhaeuser <muesli@gmail.com>
+ */
+
+// Package discordbee is
+package discordbee
+
+import (
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/muesli/beehive/bees"
+)
+
+// DiscordBee is
+type DiscordBee struct {
+	bees.Bee
+
+	apiToken      string
+	triggerPhrase string
+	eventChan     chan bees.Event
+	discord       *discordgo.Session
+}
+
+// Run executes the Bee's event loop.
+func (mod *DiscordBee) Run(eventChan chan bees.Event) {
+	mod.eventChan = eventChan
+
+	mod.Logf("Starting discord bee with apiToken %s", mod.apiToken)
+	d, err := discordgo.New("Bot " + mod.apiToken)
+	if err != nil {
+		mod.LogFatal("Unable to start discordbee! Error: ", err)
+	}
+	mod.discord = d
+	mod.Logf("Done!")
+
+	mod.discord.AddHandler(mod.isReady)
+	mod.discord.AddHandler(mod.onSend)
+
+	err = mod.discord.Open()
+
+	if err != nil {
+		mod.LogFatal("Error opening websocket: ", err)
+	}
+
+	defer mod.discord.Close()
+
+	select {
+	case <-mod.SigChan:
+		return
+	}
+}
+
+func (mod *DiscordBee) isReady(s *discordgo.Session, event *discordgo.Ready) {
+	mod.Logf("Is ready")
+	s.UpdateStatus(0, "with bees")
+}
+
+func (mod *DiscordBee) onSend(s *discordgo.Session, event *discordgo.MessageCreate) {
+	if event.Author.ID == s.State.User.ID {
+		return
+	}
+
+	if strings.HasPrefix(event.Content, mod.triggerPhrase+" ") {
+		c, e := s.Channel(event.ChannelID)
+		if e != nil {
+			mod.LogErrorf("Unable to find channel with id %s", event.ChannelID)
+			return
+		}
+		ev := bees.Event{
+			Bee:  mod.Name(),
+			Name: "message_received",
+			Options: []bees.Placeholder{
+				{
+					Name:  "contents",
+					Type:  "string",
+					Value: strings.Replace(event.Content, mod.triggerPhrase+" ", "", 1),
+				},
+				{
+					Name:  "username",
+					Type:  "string",
+					Value: event.Author.String(),
+				},
+				{
+					Name:  "channel_id",
+					Type:  "string",
+					Value: event.ChannelID,
+				},
+				{
+					Name:  "channel_name",
+					Type:  "string",
+					Value: c.Name,
+				},
+			},
+		}
+		mod.eventChan <- ev
+	}
+
+}
+
+// Action triggers the action passed to it.
+func (mod *DiscordBee) Action(action bees.Action) []bees.Placeholder {
+	outs := []bees.Placeholder{}
+	switch action.Name {
+	case "send_message":
+		var contents string
+		var channelID string
+		action.Options.Bind("contents", &contents)
+		action.Options.Bind("channel_id", &channelID)
+
+		_, err := mod.discord.ChannelMessageSend(channelID, contents)
+		if err != nil {
+			mod.LogErrorf("Unable to send message: %v", err)
+		}
+	}
+	return outs
+}
+
+// ReloadOptions parses the config options and initializes the Bee.
+func (mod *DiscordBee) ReloadOptions(options bees.BeeOptions) {
+	mod.SetOptions(options)
+
+	options.Bind("api_token", &mod.apiToken)
+	options.Bind("trigger_phrase", &mod.triggerPhrase)
+}
---
+++ bees/discordbee/discordbeefactory.go
@@ -0,0 +1,151 @@
+/*
+ *    Copyright (C) 2019      CalmBit
+ *                  2014-2019 Christian Muehlhaeuser
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Authors:
+ *      CalmBit <calmbit@posto.net>
+ *      Christian Muehlhaeuser <muesli@gmail.com>
+ */
+
+package discordbee
+
+import (
+	"github.com/muesli/beehive/bees"
+)
+
+// DiscordBeeFactory is a factory for DiscordBees.
+type DiscordBeeFactory struct {
+	bees.BeeFactory
+}
+
+// New returns a new Bee instance configured with the supplied options.
+func (factory *DiscordBeeFactory) New(name, description string, options bees.BeeOptions) bees.BeeInterface {
+	bee := DiscordBee{
+		Bee: bees.NewBee(name, factory.ID(), description, options),
+	}
+	bee.ReloadOptions(options)
+
+	return &bee
+}
+
+// ID returns the ID of this Bee.
+func (factory *DiscordBeeFactory) ID() string {
+	return "discordbee"
+}
+
+// Name returns the name of this Bee.
+func (factory *DiscordBeeFactory) Name() string {
+	return "Discord"
+}
+
+// Description returns the description of this Bee.
+func (factory *DiscordBeeFactory) Description() string {
+	return "Connects to Discord as a bot!"
+}
+
+// Image returns the filename of an image for this Bee.
+func (factory *DiscordBeeFactory) Image() string {
+	return factory.ID() + ".png"
+}
+
+// LogoColor returns the preferred logo background color (used by the admin interface).
+func (factory *DiscordBeeFactory) LogoColor() string {
+	return "#7289DA"
+}
+
+// Options returns the options available to configure this Bee.
+func (factory *DiscordBeeFactory) Options() []bees.BeeOptionDescriptor {
+	opts := []bees.BeeOptionDescriptor{
+		{
+			Name:        "api_token",
+			Description: "The Discord API token for your bot",
+			Type:        "string",
+			Mandatory:   true,
+		},
+		{
+			Name:        "trigger_phrase",
+			Description: "The phrase/word to start a command with in order for Beehive to recognize it",
+			Type:        "string",
+			Mandatory:   true,
+		},
+	}
+	return opts
+}
+
+// Events describes the available events provided by this Bee.
+func (factory *DiscordBeeFactory) Events() []bees.EventDescriptor {
+	events := []bees.EventDescriptor{
+		{
+			Namespace:   factory.Name(),
+			Name:        "message_received",
+			Description: "is triggered when a message has been recieved with the trigger word",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "contents",
+					Description: "Contents of the message",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "Name of the user who sent the message",
+					Type:        "string",
+				},
+				{
+					Name:        "channel_id",
+					Description: "ID of the channel the message was sent in",
+					Type:        "string",
+				},
+				{
+					Name:        "channel_name",
+					Description: "Name of the channel the message was sent in",
+					Type:        "string",
+				},
+			},
+		},
+	}
+	return events
+}
+
+// Actions describes the available actions provided by this Bee.
+func (factory *DiscordBeeFactory) Actions() []bees.ActionDescriptor {
+	actions := []bees.ActionDescriptor{
+		{
+			Namespace:   factory.Name(),
+			Name:        "send_message",
+			Description: "Sends a general message to the specified channel",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "contents",
+					Description: "Contents of the message",
+					Type:        "string",
+					Mandatory:   true,
+				},
+				{
+					Name:        "channel_id",
+					Description: "ID of the channel to post in",
+					Type:        "string",
+					Mandatory:   true,
+				},
+			},
+		},
+	}
+	return actions
+}
+
+func init() {
+	f := DiscordBeeFactory{}
+	bees.RegisterFactory(&f)
+}
