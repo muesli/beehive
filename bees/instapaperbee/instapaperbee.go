2f7f86f1a316f2b20fab64f4744fe07a0095999e
Adam Petrovic <apetrovic@atlassian.com>
2019-11-07 21:10:25 +1100

    Added copyright and fixed import ordering

--- bees/instapaperbee/instapaperbee.go
+++ bees/instapaperbee/instapaperbee.go
@@ -1,9 +1,30 @@
+/*
+ *    Copyright (C) 2019 Adam Petrovic
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Authors:
+ *      Adam Petrovic <adam@petrovic.com.au>
+ */
+
 package instapaperbee
 
 import (
-	"github.com/muesli/beehive/bees"
-	"net/url"
 	"net/http"
+	"net/url"
+
+	"github.com/muesli/beehive/bees"
 )
 
 type InstapaperBee struct {
