c138f1a6355bf11b66b841e8e1c77d6e8309a1cb
Nicolas Martin <penguwin@penguwin.eu>
2020-01-05 01:21:17 +0100

    Updated MastodonBee to use the UserStream events
    This bee now supports a variety of new events:
    - `toot_fetched`: is triggered whenever a Toot was fetched. (includes your own
    Toots and Toots from people you follow)
    - `deleted`: is triggerd when a Toot has been deleted from Mastodon. (Includes
    deletion of Toots from people you follow)
    - `follow`: is triggered when a user wants to follow you on mastodon.
    - `favourite`: is triggered when a user favourites your toot.
    - `reblog`: is triggered when a user reblogs on of your toots.
    - `mention`: is triggered when a user mentions you on mastodon.

---
+++ bees/mastodonbee/events.go
@@ -0,0 +1,244 @@
+/*
+ *    Copyright (C) 2020 Christian Muehlhaeuser
+ *                  2020 Nicolas Martin
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published
+ *    by the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    Authors:
+ *      Christian Muehlhaeuser <muesli@gmail.com>
+ *      Nicolas Martin <penguwin@penguwin.eu>
+ */
+package mastodonbee
+
+import (
+	"context"
+
+	mastodon "github.com/mattn/go-mastodon"
+	"github.com/muesli/beehive/bees"
+)
+
+// handleUpdateEvent handles incoming Toot updates from mastodon from yourself
+// and from people you follow.
+func (mod *mastodonBee) handleStatus(status *mastodon.Status) {
+	ev := bees.Event{
+		Bee:  mod.Name(),
+		Name: "toot_fetched",
+		Options: []bees.Placeholder{
+			{
+				Name:  "id",
+				Value: string(status.ID),
+				Type:  "string",
+			},
+			{
+				Name:  "text",
+				Value: status.Content,
+				Type:  "string",
+			},
+			{
+				Name:  "user_id",
+				Value: string(status.Account.ID),
+				Type:  "string",
+			},
+			{
+				Name:  "username",
+				Value: status.Account.DisplayName,
+				Type:  "string",
+			},
+			{
+				Name:  "reblogs",
+				Value: status.ReblogsCount,
+				Type:  "int64",
+			},
+			{
+				Name:  "favourites",
+				Value: status.FavouritesCount,
+				Type:  "int64",
+			},
+			{
+				Name:  "url",
+				Value: status.URL,
+				Type:  "string",
+			},
+		},
+	}
+	mod.evchan <- ev
+}
+
+func (mod *mastodonBee) handleNotification(notif *mastodon.Notification) {
+	switch notif.Type {
+
+	case "follow":
+		rel, err := mod.client.GetAccountRelationships(context.Background(), []string{string(notif.Account.ID)})
+		if err != nil {
+			mod.LogErrorf("Failed to fetch account relationship at follow event: %v", err)
+			return
+		}
+
+		if len(rel) == 0 {
+			// NOTE: Does this even happen?
+			// TODO: Investigate
+			mod.LogErrorf("No relationship information was fetched")
+			return
+		}
+
+		// NOTE: there should be only one relationship entity fetched
+		ev := bees.Event{
+			Bee:  mod.Name(),
+			Name: "follow",
+			Options: []bees.Placeholder{
+				{
+					Name:  "user_id",
+					Value: string(notif.Account.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "username",
+					Value: notif.Account.DisplayName,
+					Type:  "string",
+				},
+				{
+					Name:  "following",
+					Value: rel[0].Following,
+					Type:  "bool",
+				},
+				{
+					Name:  "followed_by",
+					Value: rel[0].FollowedBy,
+					Type:  "bool",
+				},
+				{
+					Name:  "followers",
+					Value: notif.Account.FollowersCount,
+					Type:  "int64",
+				},
+				{
+					Name:  "follows",
+					Value: notif.Account.FollowingCount,
+					Type:  "int64",
+				},
+			},
+		}
+		mod.evchan <- ev
+
+	case "favourite":
+		ev := bees.Event{
+			Bee:  mod.Name(),
+			Name: "favourite",
+			Options: []bees.Placeholder{
+				{
+					Name:  "id",
+					Value: string(notif.Status.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "user_id",
+					Value: string(notif.Account.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "username",
+					Value: notif.Account.DisplayName,
+					Type:  "string",
+				},
+				{
+					Name:  "text",
+					Value: notif.Status.Content,
+					Type:  "string",
+				},
+				{
+					Name:  "url",
+					Value: notif.Status.URL,
+					Type:  "string",
+				},
+				{
+					Name:  "favourites",
+					Value: notif.Status.FavouritesCount,
+					Type:  "int64",
+				},
+			},
+		}
+		mod.evchan <- ev
+
+	case "reblog":
+		ev := bees.Event{
+			Bee:  mod.Name(),
+			Name: "reblog",
+			Options: []bees.Placeholder{
+				{
+					Name:  "user_id",
+					Value: string(notif.Account.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "username",
+					Value: notif.Status.Account.DisplayName,
+					Type:  "string",
+				},
+				{
+					Name:  "text",
+					Value: notif.Status.Content,
+					Type:  "string",
+				},
+				{
+					Name:  "url",
+					Value: notif.Status.URL,
+					Type:  "string",
+				},
+				{
+					Name:  "reblogs",
+					Value: notif.Status.ReblogsCount,
+					Type:  "int64",
+				},
+			},
+		}
+		mod.evchan <- ev
+
+	case "mention":
+		ev := bees.Event{
+			Bee:  mod.Name(),
+			Name: "mention",
+			Options: []bees.Placeholder{
+				{
+					Name:  "id",
+					Value: string(notif.Status.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "user_id",
+					Value: string(notif.Account.ID),
+					Type:  "string",
+				},
+				{
+					Name:  "username",
+					Value: notif.Status.Account.DisplayName,
+					Type:  "string",
+				},
+				{
+					Name:  "text",
+					Value: notif.Status.Content,
+					Type:  "string",
+				},
+				{
+					Name:  "url",
+					Value: notif.Status.URL,
+					Type:  "string",
+				},
+			},
+		}
+		mod.evchan <- ev
+
+	default:
+		mod.LogErrorf("Unkown notification type: %s", notif.Type)
+	}
+}
--- bees/mastodonbee/mastodonbeefactory.go
+++ bees/mastodonbee/mastodonbeefactory.go
@@ -16,7 +16,7 @@
  *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  *    Authors:
- *      Nicolas Martin <penguwin@systemli.org>
+ *      Nicolas Martin <penguwin@penguwin.eu>
  *      Christian Muehlhaeuser <muesli@gmail.com>
  */
 
@@ -106,12 +106,192 @@ func (factory *mastodonBeeFactory) Events() []bees.EventDescriptor {
 	events := []bees.EventDescriptor{
 		{
 			Namespace:   factory.Name(),
-			Name:        "toot_sent",
-			Description: "A toot has been sent",
+			Name:        "deleted",
+			Description: "is triggered when a toot has been deleted",
 			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "id",
+					Description: "The ID of the deleted toot",
+					Type:        "string",
+				},
+			},
+		},
+		{
+			Namespace:   factory.Name(),
+			Name:        "toot_fetched",
+			Description: "is triggered when a toot has been fetched",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "id",
+					Description: "The ID of the toot",
+					Type:        "string",
+				},
 				{
 					Name:        "text",
 					Description: "Text of the toot that has been sent",
+					Type:        "string",
+				},
+				{
+					Name:        "user_id",
+					Description: "Mastodon ID if the toots author",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "Mastodon handle of the toots author",
+					Type:        "string",
+				},
+				{
+					Name:        "reblogs",
+					Description: "reblogs count",
+					Type:        "int64",
+				},
+				{
+					Name:        "favourites",
+					Description: "favourites count",
+					Type:        "int64",
+				},
+				{
+					Name:        "url",
+					Description: "The url for the toot",
+					Type:        "string",
+				},
+			},
+		},
+		{
+			Namespace:   factory.Name(),
+			Name:        "follow",
+			Description: "is triggered when someone wants to follow you",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "user_id",
+					Description: "Mastodon ID of the user which triggered the follow event",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "Mastodon handle of the user which triggered the follow event",
+					Type:        "string",
+				},
+				{
+					Name:        "following",
+					Description: "Indicates if your're following the user",
+					Type:        "bool",
+				},
+				{
+					Name:        "followed_by",
+					Description: "Indicates if you're followed by the user",
+					Type:        "bool",
+				},
+				{
+					Name:        "followers",
+					Description: "Number of followers for the user which triggered the follow request",
+					Type:        "int64",
+				},
+				{
+					Name:        "follows",
+					Description: "Number of follows for the user which triggered the follow request",
+					Type:        "int64",
+				},
+			},
+		},
+		{
+			Namespace:   factory.Name(),
+			Name:        "favourite",
+			Description: "is triggered when someone favourites one of your toots.",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "id",
+					Description: "The ID of toot",
+					Type:        "string",
+				},
+				{
+					Name:        "user_id",
+					Description: "Mastodon ID of the user that favourited your toot",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "The Mastodon handle of the user that favourited your toot",
+					Type:        "string",
+				},
+				{
+					Name:        "text",
+					Description: "text content of the favourited toot",
+					Type:        "string",
+				},
+				{
+					Name:        "url",
+					Description: "URL of the favourited toot",
+					Type:        "string",
+				},
+				{
+					Name:        "favourites",
+					Description: "The count of favourites for this toot",
+					Type:        "int64",
+				},
+			},
+		},
+		{
+			Namespace:   factory.Name(),
+			Name:        "reblog",
+			Description: "is triggered when someone reblogs on of your toots",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "user_id",
+					Description: "Mastodon ID of the user tht reblogged your toot",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "Mastodon handle of the user that reblogged your toot",
+					Type:        "string",
+				},
+				{
+					Name:        "text",
+					Description: "text content of the mention",
+					Type:        "string",
+				},
+				{
+					Name:        "url",
+					Description: "URL of the mention",
+					Type:        "string",
+				},
+				{
+					Name:        "reblogs",
+					Description: "Number of reblogs for the post",
+					Type:        "string",
+				},
+			},
+		},
+		{
+			Namespace:   factory.Name(),
+			Name:        "mention",
+			Description: "is triggered whenever someone mentions you on Mastodon",
+			Options: []bees.PlaceholderDescriptor{
+				{
+					Name:        "id",
+					Description: "The ID of toot",
+					Type:        "string",
+				},
+				{
+					Name:        "user_id",
+					Description: "Mastodon ID if the mention's author",
+					Type:        "string",
+				},
+				{
+					Name:        "username",
+					Description: "The Mastodon handle of the mention's author",
+					Type:        "string",
+				},
+				{
+					Name:        "text",
+					Description: "text content of the mention",
+					Type:        "string",
+				},
+				{
+					Name:        "url",
+					Description: "URL of the mention",
 					Type:        "string",
 				},
 			},
