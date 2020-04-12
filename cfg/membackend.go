0fb6166fe10e8d22cff35ff177ff8caefa70b4c7
Sergio Rubio <rubiojr@frameos.org>
2020-04-12 18:52:57 +0200

    Mem backend fixes

--- cfg/membackend.go
+++ cfg/membackend.go
@@ -9,16 +9,22 @@ func (mb *MemBackend) URI() string {
 	return mb.uri.String()
 }
 
-func (mb MemBackend) SetURI(uri string) error {
+func (mb *MemBackend) SetURI(uri string) error {
+	url, err := url.Parse(uri)
+	if err != nil {
+		return err
+	}
+
+	mb.uri = url
 	return nil
 }
 
 // Load loads chains from config
-func (m MemBackend) Load() (Config, error) {
+func (m *MemBackend) Load() (Config, error) {
 	return Config{}, nil
 }
 
 // Save saves chains to config
-func (m MemBackend) Save(config Config) error {
+func (m *MemBackend) Save(config Config) error {
 	return nil
 }
