06ab7fab5f16e77c62f31b1278c90bf22eef6e0a
Sergio Rubio <rubiojr@frameos.org>
2020-05-07 20:50:50 +0200

    Empbedding API

---
+++ reactor/run.go
@@ -0,0 +1,54 @@
+package reactor
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/muesli/beehive/bees"
+	"github.com/muesli/beehive/cfg"
+)
+
+// Reactor loops and handles signals.
+func Run(config *cfg.Config) {
+	// Load actions from config
+	bees.SetActions(config.Actions)
+	// Load chains from config
+	bees.SetChains(config.Chains)
+	// Initialize bees
+	bees.StartBees(config.Bees)
+
+	// Wait for signals
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt, syscall.SIGHUP, syscall.SIGTERM, syscall.SIGKILL)
+
+	for s := range ch {
+		log.Println("Got signal:", s)
+
+		abort := false
+		switch s {
+		case syscall.SIGHUP:
+			err := config.Load()
+			if err != nil {
+				log.Panicf("Error loading config from %s: %v", config.URL(), err)
+			}
+			bees.StopBees()
+			bees.SetActions(config.Actions)
+			bees.SetChains(config.Chains)
+			bees.StartBees(config.Bees)
+
+		case syscall.SIGTERM:
+			fallthrough
+		case syscall.SIGKILL:
+			fallthrough
+		case os.Interrupt:
+			abort = true
+			break
+		}
+
+		if abort {
+			break
+		}
+	}
+}
