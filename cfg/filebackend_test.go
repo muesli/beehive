94fc48cf8395fc9bd8c6114090e1cd2f4985e177
Sergio Rubio <rubiojr@frameos.org>
2020-05-10 15:36:04 +0200

    :lipstick:

--- cfg/filebackend_test.go
+++ cfg/filebackend_test.go
@@ -1,8 +1,11 @@
 package cfg
 
 import (
+<<<<<<< HEAD
 	"io/ioutil"
 	"net/url"
+=======
+>>>>>>> :lipstick:
 	"os"
 	"path/filepath"
 	"testing"
@@ -53,11 +56,6 @@ func TestFileLoad(t *testing.T) {
 }
 
 func TestFileSave(t *testing.T) {
-	tmpdir, err := ioutil.TempDir("", "beehivetest")
-	if err != nil {
-		t.Error("Could not create temp directory")
-	}
-
 	u, err := ParseURL(filepath.Join("testdata", "beehive.conf"))
 	if err != nil {
 		t.Fatalf("Can't parse URL. %v", err)
@@ -92,7 +90,8 @@ func TestFileSave(t *testing.T) {
 	}
 
 	// Save the config file to a new absolute path using a regular path
-	p = filepath.Join(tmpdir, "beehive.conf")
+	p = tmpConfPath()
+	c.SetURL(p)
 	u, err = ParseURL(p)
 	if err != nil {
 		t.Error("cannot parse url")
